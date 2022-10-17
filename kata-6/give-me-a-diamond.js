21d3f3ee687f8f4dd1d0cb537c988c0db65367c9
Erkhembayar-Atarbold <erkhembayar.atarbold.dev@gmail.com>
2022-10-17 00:32:52 -0700

    Created a me kata

---
+++ kata-6/give-me-a-diamond.js
@@ -0,0 +1,27 @@
+//Create a function that will print a string * shape of a diamond
+//Based on the argument int the shape of the diamond will be determined by the statement
+//Int is + or -
+
+function createDiamond(int) {
+  //   let diamond = "";
+  //condition
+  //If to find if argument is a - or even
+  //return a null
+  //Else
+  //for
+  //   if (int < 1 && int % 2 === 0) {
+  //     return null;
+  //   } else {
+  //     let mid = Math.ceil(int / 2);
+  //     for (let i = 0; i <= int; i++) {
+  //       diamond += "".repeat(mid - i) + ;
+  //     }
+  //   }
+  //   return diamond;
+}
+
+console.log(createDiamond(1));
+
+console.log(createDiamond(3));
+console.log(createDiamond(10));
+console.log(createDiamond(15));
---
+++ kata-7/two-fighter-one-winner.js
@@ -0,0 +1,40 @@
+//Create a function that returns the name of the winner
+
+//A class applied to create fighter objects
+
+class Figher {
+  constructor(name, health, damagePerAttac) {
+    this.name = name;
+    this.health = health;
+    this.damagePerAttac = damagePerAttac;
+  }
+}
+
+const fighterOne = new Figher("satami", 10, 8);
+
+const fighterTwo = new Figher("Joji", 10, 1);
+
+//Function will have three parameters
+//First and second parameter will be a Fighter object
+//The third parameter will be the attacker
+
+function whoIsWinner(fighter1, fighter2, firstStrike) {
+  while (fighter1.health > 1 && fighter2.health > 1) {
+    if (firstStrike === fighter1.name) {
+      fighter1.health -= fighter2.damagePerAttac;
+      fighter2.health -= fighter1.damagePerAttac;
+      if (fighter1.health <= 0) {
+        return fighter1.health;
+      } else if (fighter2.health <= 0) {
+        return fighter2.name;
+      }
+    } else if (firstStrike === fighter2.name) {
+      fighter2.health -= fighter1.damagePerAttac;
+      fighter1.health -= fighter2.damagePerAttac;
+    }
+    console.log(fighter1.health);
+    console.log(fighter2.health);
+  }
+}
+
+console.log(whoIsWinner(fighterOne, fighterTwo, fighterOne.name));
