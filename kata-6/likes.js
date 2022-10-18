5337259b007c62501ad559e6f38b91bad892c01a
Erkhembayar-Atarbold <erkhembayar.atarbold.dev@gmail.com>
2022-10-18 09:00:19 -0700

    Likes.js solution created

--- kata-6/likes.js
+++ kata-6/likes.js
@@ -1,2 +1,21 @@
 //Create a function takes the names of people who like an item and return display text
-function likes(userName) {}
+function likes(userName) {
+  const likeOne = "likes this";
+
+  const likePlural = "like this";
+  if (userName.length === 1) {
+    return userName + " " + likeAdd;
+  } else if (userName.length === 2) {
+    return `${userName[0]} and ${userName[1]} ${likePlural} `;
+  } else if (userName.length === 3) {
+    return `${userName[0]}, ${userName[1]} and ${userName[2]} ${likePlural}`;
+  } else if (userName.length >= 4) {
+    return `${userName[0]}, ${userName[1]} and ${
+      userName.length - 2
+    } others ${likePlural}`;
+  } else {
+    return "no one likes this";
+  }
+}
+
+console.log(likes([]));
