5a05daf85a6846ede217571be840ed9e5afe7bb8
Erkhembayar-Atarbold <erkhembayar.atarbold.dev@gmail.com>
2022-10-21 06:14:06 -0700

    The solution to powers of two kata found

---
+++ kata-8/powersOfTwo.js
@@ -0,0 +1,13 @@
+//The function should be able to find the powers of a non-negative number and return it in array
+
+function findPowers(number) {
+  let array = [];
+
+  for (let i = 0; i <= number; i++) {
+    array.push(Math.pow(2, i));
+  }
+
+  return array;
+}
+
+console.log(findPowers(0));
