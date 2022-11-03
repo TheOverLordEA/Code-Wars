8216efb39a60457c3c106c5f649df3a157a8d6f9
Erkhembayar-Atarbold <erhembold@gmail.com>
2022-11-03 05:17:03 -0700

    maxSquence file answer commented

--- kata-5/maxSquence.js
+++ kata-5/maxSquence.js
@@ -1,3 +1,26 @@
 //Create a function that locates the continous subsequence in an array
 
-function locateSquence(arr) {}
+// function locateSquence(arr) {
+//   //Arr is empty return zerp
+//   if (arr === []) {
+//     return 0;
+//   }
+
+//   const newA = [];
+//   //Loop
+//   const arrAnswer = arr.every((ele) => ele < 0);
+
+//   if (arrAnswer) {
+//     return 0;
+//   } else {
+//     for (let i = 0; i < arr.length; i++) {
+//       for (let y = i + 1; y < arr.length; y++) {
+
+//       }
+//       console.log(arr[i]);
+//     }
+//   }
+//   console.log(newA);
+// }
+
+// locateSquence([-2, 1, -3, 4, -1, 2, 1, -5, 4]);
