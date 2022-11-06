b3930e830ad017c100625bc0bf970795816e86ff
Erkhembayar-Atarbold <erhembold@gmail.com>
2022-11-06 05:04:57 -0800

    Altered kyu 5 and created the answer for messi.js

---
+++ kata-8/messiScore.js
@@ -0,0 +1,7 @@
+//Create a function that will find the sum of goals made by Messi in the laLiga, Champions, and CopaDelRay
+
+const laLiga = 43;
+const CopaDelRay = 5;
+const championsLeague = 10;
+
+const points = laLiga + CopaDelRay + championsLeague;
