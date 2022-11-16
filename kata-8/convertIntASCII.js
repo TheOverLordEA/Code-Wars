f634ca15d9cf45627d350db5149a04cab8fac972
Erkhembayar-Atarbold <erhembold@gmail.com>
2022-11-15 23:52:11 -0800

    Created a solution for converASCII.js

---
+++ kata-8/convertIntASCII.js
@@ -0,0 +1,7 @@
+//Write a function that will convert a int to a string based on ASCII table
+
+function convertNumber(num) {
+  return String.fromCharCode(num);
+}
+
+// console.log(convertNumber(50));
