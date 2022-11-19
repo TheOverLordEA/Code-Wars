2eac363b1abb60a94efe5ccb11be55d4779b4808
Erkhembayar-Atarbold <erhembold@gmail.com>
2022-11-19 05:45:49 -0800

    Built solution for oneTwo.js

---
+++ kata-7/oneTwo.js
@@ -0,0 +1,7 @@
+//Create a function that will take a string and add the correct number
+
+function oneTwo(str) {
+  return str.map((el, i) => `${i + 1}: ${el}`);
+}
+
+console.log(oneTwo(["a", "b"]));
