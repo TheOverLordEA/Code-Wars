ce5ebea76952741f6e111da14689d9853be0d418
Erkhembayar-Atarbold <erhembold@gmail.com>
2022-11-22 05:42:30 -0800

    Built solution kata8 iLoveYouAlot

--- kata-8/iLoveYouAlot.js
+++ kata-8/iLoveYouAlot.js
@@ -1,5 +1,68 @@
 //Write a function that returns a string based on the pedal
 
-function getHowMuchLove(int) {}
+function getHowMuchLove(int) {
+  const array = [
+    "I love you",
+    "a little",
+    "a lot",
+    "passionately",
+    "madly",
+    "not at all",
+  ];
 
-console.log(getHowMuchLove(1));
+  let modifiefdArray = [];
+
+  let i = 0;
+
+  let y = 0;
+
+  while (i < int) {
+    modifiefdArray.push(array[i]);
+    if (array[i] === undefined) {
+      modifiefdArray.splice(i);
+      modifiefdArray.push(array[y]);
+      y++;
+      if (y >= 6) {
+        y = 0;
+      }
+    }
+    i++;
+  }
+  return modifiefdArray[int - 1];
+}
+
+console.log(getHowMuchLove(10));
+
+// function howMuchILoveYou(nbPetals) {
+//   // your code
+//   const array = [
+//     "I love you",
+//     "a little",
+//     "a lot",
+//     "passionately",
+//     "madly",
+//     "not at all",
+//   ];
+
+//   let modifiefdArray = [];
+
+//   let i = 0;
+
+//   let y = 0;
+
+//   while (i < nbPetals) {
+//     modifiefdArray.push(array[i]);
+//     if (array[i] === undefined) {
+//       modifiefdArray.splice(i);
+//       modifiefdArray.push(array[y]);
+//       y++;
+//       if (y >= 6) {
+//         y = 0;
+//       }
+//     }
+//     i++;
+//   }
+//   return modifiefdArray[nbPetals];
+// }
+
+// console.log(howMuchILoveYou(1));
