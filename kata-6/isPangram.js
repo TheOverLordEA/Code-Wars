75e91a88d09691dd4c09cc51e8d5cb708ad45c19
Erkhembayar-Atarbold <erhembold@gmail.com>
2022-12-08 03:05:50 -0800

    Solution found for isPangram kata

---
+++ kata-6/isPangram.js
@@ -0,0 +1,14 @@
+//Write a function that will determine if argument is a pangram
+
+function isPangram(string) {
+  string = string.toLowerCase();
+  return "abcdefghijklmnopqrstuvwxyz".split("").every(function (x) {
+    return string.indexOf(x) !== -1;
+  });
+}
+
+console.log(
+  isPangram(
+    "A pangram is a sentence that contains every single letter of the alphabet at least once."
+  )
+);
