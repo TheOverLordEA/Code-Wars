863a3726abe20af7a5fe711a7c83a38b5ee3210a
Erkhembayar-Atarbold <erhembold@gmail.com>
2022-12-09 00:33:51 -0800

    Built answer for printerError.js

--- kata-7/printerError.js
+++ kata-7/printerError.js
@@ -1,1 +1,38 @@
 //Write a function that will return tha fail rate of a printer as string
+
+function printerError(printerCode) {
+  const sanitize = printerCode.split("");
+  //Counter var
+  let letterCounter = 0;
+
+  //Create a array to store a to m
+
+  const aToM = [
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+
+  //Create loop
+
+  for (let i = 0; i < aToM.length; i++) {
+    for (let b = 0; b < sanitize.length; b++) {
+      if (aToM[i] === sanitize[b]) {
+        letterCounter++;
+      }
+    }
+  }
+
+  return `${letterCounter}/${sanitize.length}`;
+}
+console.log(printerError("aaaxbbbbyyhwawiwjjjwwm"));
