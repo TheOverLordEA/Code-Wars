64113746fe5ec043bdf0c914624a3038d43cf679
Erkhembayar-Atarbold <erhembold@gmail.com>
2022-12-31 06:45:02 -0800

    Found solution to seriesSum

--- kata-7/trainFirst.js
+++ kata-7/trainFirst.js
@@ -1,3 +1,17 @@
 //Create a function that will return the decimal point of a int to string
 
-function seriesSum(int) {}
+function seriesSum(int) {
+  let result = 0;
+  let reverage = 1;
+  for (let i = 0; i < int; i += 1) {
+    if (i === 0) {
+      result = 1;
+    } else {
+      reverage += 3;
+      result = result + 1 / reverage;
+    }
+  }
+  return Number(result.toFixed(2));
+}
+
+console.log(seriesSum(2));
