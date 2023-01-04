0fae75d4f25541825b94dc04b1bf4b3ac1403ad8
Erkhembayar-Atarbold <erhembold@gmail.com>
2023-01-03 22:11:58 -0800

    Created file for sortNumbers

---
+++ kata-7/sortNumbers.js
@@ -0,0 +1,13 @@
+//Create a function that will sort an array containing integers
+
+function solution(arr) {
+  //   if (!arr) {
+  //     return [];
+  //   } else {
+  //     return arr.sort((a, b) => a - b);
+  //   }
+
+  return arr || [];
+}
+
+console.log(solution());
