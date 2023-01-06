ad06b0261a3ff574f14a4967326687f7650bfce8
Erkhembayar-Atarbold <erhembold@gmail.com>
2023-01-06 01:40:04 -0800

    The file for girlFriendDiamond.js built

---
+++ kata-6/girlFriendDiamond.js
@@ -0,0 +1,10 @@
+//Create a function that will print * depending on the number
+
+function solution(number) {
+  if (number % 2 === 0 || number < 0) {
+    return null;
+  } else {
+  }
+}
+
+console.log(solution(5));
