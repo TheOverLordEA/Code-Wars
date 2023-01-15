4263333c1df5939089a26938daf77e298023caef
Erkhembayar-Atarbold <erhembold@gmail.com>
2023-01-14 22:41:42 -0800

    Answer for arrayFlattenSort.js created

--- kata-7/arrayFlattenSort.js
+++ kata-7/arrayFlattenSort.js
@@ -1,10 +1,7 @@
 //Create a function that will flatten array and sort based ascending order int
 
 function arrayFlattenSort(arr) {
-  //Turn array to one array using concat
-  //Sort array
-
-  return arr.map((el) => el);
+  return arr.flat().sort((a, b) => a - b);
 }
 
 console.log(arrayFlattenSort([[1, 3, 5], [100], [2, 4, 6]]));
