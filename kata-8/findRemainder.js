d35b6e7dfd7dd07511e19487f48827a35c95e15f
Erkhembayar-Atarbold <erhembold@gmail.com>
2023-01-18 04:17:23 -0800

    Final version of solution created

--- kata-8/findRemainder.js
+++ kata-8/findRemainder.js
@@ -1,5 +1,6 @@
 //Create a function that will find the remainder of the int
 
 function solution(e, u) {
-  return [e, u].sort((a, b) => a - b);
+  const array = [e, u].sort((a, b) => a - b);
+  return array[1] % array[0];
 }
